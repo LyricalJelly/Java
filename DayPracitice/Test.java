34f9facb3171bfcaaa1ccb413e0f9102deb2795e
余暮风影 <1550571781@qq.com>
2021-02-02 15:00:07 +0800

    Add MyArrayList/MyArrayList/MyArrayList.java Modify DayPracitice/Test.java

--- DayPracitice/Test.java
+++ DayPracitice/Test.java
@@ -3,4 +3,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    public static int getImportance(List<Employee> employees, int id) {
+
+    }
 }
---
+++ MyArrayList/MyArrayList/MyArrayList.java
@@ -0,0 +1,29 @@
+package MyArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
+public class MyArrayList<T> implements Iterable<T> {
+
+    private T[] array;
+
+    private int capacity;
+
+    private int size;
+
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    // public static <T,U> T[] copyOf(U[] original, int newLength, Class<? extends
+    // T[]> newType)
+    public static void main(String[] args) {
+
+    }
+
+}
