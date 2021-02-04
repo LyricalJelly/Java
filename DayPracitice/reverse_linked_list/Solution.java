624538edcc0805cc92ff0e2f452433bf36ed927a
余暮风影 <1550571781@qq.com>
2021-02-04 14:29:36 +0800

    add DayPracitice/remove_linked_list_elements/Solution.java add DayPracitice/reverse_linked_list/Solution.java

---
+++ DayPracitice/reverse_linked_list/Solution.java
@@ -0,0 +1,28 @@
+package DayPracitice.reverse_linked_list;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode reverseList(ListNode head) {
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
